b47d3d74ce6c6941480fc4eca7ceb5f043b27d88
“date” <1186357024@qq.com>
2023-06-05 22:27:27 +0800

    6.5

---
+++ LeetCode/src/Arithmetic/Dp/C1_CompletePacking.java
@@ -0,0 +1,47 @@
+package Arithmetic.Dp;
+
+/**
+ * 背包最大重量为4。
+ *
+ * 物品为：
+ *
+ *         重量	价值
+ * 物品  0	1	15
+ * 物品  1	3	20
+ * 物品  2	4	30
+ * 每件商品都有无限个！
+ *
+ * 问背包能背的物品最大价值是多少？
+ *
+ * 01背包和完全背包唯一不同就是体现在遍历顺序上，所以本文就不去做动规五部曲了，我们直接针对遍历顺序经行分析！
+ *
+ * 其实还有一个很重要的问题，为什么遍历物品在外层循环，遍历背包容量在内层循环？
+ * 这个问题很多题解关于这里都是轻描淡写就略过了，大家都默认 遍历物品在外层，遍历背包容量在内层，好像本应该如此一样，那么为什么呢？
+ * 01背包中二维dp数组的两个for遍历的先后循序是可以颠倒了，
+ * 一维dp数组的两个for循环先后循序一定是先遍历物品，再遍历背包容量。
+ *
+ * 在完全背包中，对于一维dp数组来说，其实两个for循环嵌套顺序是无所谓的！
+ * 因为dp[j] 是根据 下标j之前所对应的dp[j]计算出来的。
+ * 只要保证下标j之前的dp[j]都是经过计算的就可以了。
+ *
+ * 完全背包中，两个for循环的先后循序，都不影响计算dp[j]所需要的值（这个值就是下标j之前所对应的dp[j]）。
+ * 先遍历背包在遍历物品，代码如下：
+ */
+public class C1_CompletePacking {
+    public static void main(String[] args) {
+        int[] weight = {1,3,4};
+        int[] value = {15,20,30};
+        int bagSize = 4;
+        C1_CompletePacking c1_completePacking = new C1_CompletePacking();
+        System.out.println(c1_completePacking.completePacking(weight, value, bagSize));
+    }
+    public int completePacking(int[] weight, int[] value, int bagSize){
+        int[] dp = new int[bagSize + 1];
+        for (int i = 0; i < weight.length; i++) {
+            for (int j = weight[i]; j < dp.length; j++) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
+            }
+        }
+        return dp[bagSize];
+    }
+}
---
+++ LeetCode/src/Arithmetic/Dp/C2_ChangeExchangeII.java
@@ -0,0 +1,105 @@
+package Arithmetic.Dp;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+ * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+ * 假设每一种面额的硬币有无限个。
+ * 题目数据保证结果符合 32 位带符号整数。
+ *
+ * 示例 1：
+ * 输入：amount = 5, coins = [1, 2, 5]
+ * 输出：4
+ * 解释：有四种方式可以凑成总金额：
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ *
+ * 示例 2：
+ * 输入：amount = 3, coins = [2]
+ * 输出：0
+ * 解释：只用面额 2 的硬币不能凑成总金额 3 。
+ *
+ * 示例 3：
+ * 输入：amount = 10, coins = [10]
+ * 输出：1
+ *
+ * 提示：
+ * 1 <= coins.length <= 300
+ * 1 <= coins[i] <= 5000
+ * coins 中的所有值 互不相同
+ * 0 <= amount <= 5000
+ *
+ *          0  1  2  3  4  5  6
+ * coin 1   1  1  1  1  1  1  1
+ * coin 2   1  1  2  2  3  3  4
+ * coin 5   1  1  2  2  3  4  5
+ *
+ *          0  1  2  3
+ * coin 2   1  1  1  0
+ */
+public class C2_ChangeExchangeII {
+    public static void main(String[] args) {
+        C2_ChangeExchangeII c2_changeExchangeII = new C2_ChangeExchangeII();
+        int amount = 6;
+        int[] coins = new int[]{1,2,5};
+        System.out.println(c2_changeExchangeII.change(amount, coins));
+        System.out.println(c2_changeExchangeII.change1(amount, coins));
+    }
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[amount + 1][coins.length];
+        dp[0][0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = (coins[0] == 1 || i % coins[0] == 0) ? 1 : 0;//替换一
+        }
+        for (int i = 1; i < coins.length; i++) {
+            for (int j = 0; j < dp.length; j++) {
+                dp[j][i] = (j == 0) ?
+                        1 : (j < coins[i]) ?
+                        dp[j][i - 1] : dp[j - coins[i]][i] + dp[j][i - 1];//替换二
+            }
+        }
+        //三
+        return dp[amount][coins.length - 1];
+    }
+    public int change1(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = (coins[0] == 1 || i % coins[0] == 0) ? 1 : 0;
+        }
+        for (int i = 1; i < coins.length; i++) {
+            for (int j = 0; j < dp.length; j++) {
+                dp[j] = (j == 0) ?
+                        1 : (j < coins[i]) ?
+                        dp[j] : dp[j] + dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+}
+//一
+//            if (coins[0] == 1){
+//                dp[i][0] = 1;
+//            }else if (coins[0] % i == 0){
+//                dp[i][0] = 1;
+//            }
+
+//二
+//                if (j == 0){
+//                    dp[j][i] = 1;
+//                }else if (j < coins[i]){
+//                    dp[j][i] = dp[j][i - 1];
+//                }else {
+//                    dp[j][i] = dp[j - coins[i]][i] + dp[j][i - 1];
+//                }
+
+//三
+//        for (int i = 0; i < dp[0].length; i++) {
+//            for (int[] ints : dp) {
+//                System.out.print(ints[i] + " ");
+//            }
+//            System.out.println();
+//        }
